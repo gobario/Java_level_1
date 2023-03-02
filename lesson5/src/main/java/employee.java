e2ce521a7bc76f5674c615a8e1f147fc72d7f4f7
gobario <dlavova@mail.ru>
2023-03-02 14:16:28 +0500

    lesson5 done

--- lesson5/src/main/java/employee.java
+++ lesson5/src/main/java/employee.java
@@ -1,2 +1,19 @@
 public class employee {
+    private String name;
+    private String post;
+    private String mail;
+    private String mobile;
+    private int salary;
+    public int age;
+    public employee(String name, String post, String mail, String mobile, int salary, int age) {
+        this.name = name;
+        this.post = post;
+        this.mail = mail;
+        this.mobile = mobile;
+        this.salary = salary;
+        this.age = age;
+    }
+    public void conc() {
+        System.out.println("Имя: " + name + " Должность: " + post + " Почта: " + mail + " Телефон: " + mobile + " Зарплата: " + salary + " Возраст: " + age);
+    }
 }
--- lesson5/src/main/java/lesson5.java
+++ lesson5/src/main/java/lesson5.java
@@ -1,2 +1,15 @@
 public class lesson5 {
+    public static void main(String[] args) {
+        employee[] persArray = new employee[5];
+        persArray[0] = new employee("Dmitrii", "QA", "123@123", "32155", 15, 14);
+        persArray[1] = new employee("Denis", "Hearthstone", "321@123", "5466", 0, 16);
+        persArray[2] = new employee("Muzaffar", "God", "MA@123", "999", 150, 42);
+        persArray[3] = new employee("Lev", "Billionaire", "leon@123", "null", 1500, 48);
+        persArray[4] = new employee("Chubok", "Anime", "sos@123", "999987", 15, 18);
+        for (int i = 0; i < persArray.length; i++) {
+            if (persArray[i].age > 40) {
+                persArray[i].conc();
+            }
+        }
+    }
 }

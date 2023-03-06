e9da0dc59d87105d674b5723363453e62c0b70f7
gobario <dlavova@mail.ru>
2023-03-06 12:40:52 +0500

    homework6

--- homework6/src/main/java/cat.java
+++ homework6/src/main/java/cat.java
@@ -2,4 +2,5 @@ public class cat extends animal {
     public cat(String name, int length) {
         super(name, length);
     }
+
     }
--- homework6/src/main/java/homework6.java
+++ homework6/src/main/java/homework6.java
@@ -1,8 +1,5 @@
 public class homework6 {
     public static void main(String[] args) {
-//        animal animal1 = new animal("Бобик", 15);
-//        animal1.animalRun();
-//        animal1.animalSwim();
         cat cat1 = new cat("Пушистик", 25);
         cat1.animalRun();
         dog dog1 = new dog("Шарик", 33);

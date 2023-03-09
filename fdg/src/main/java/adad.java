4dd3a84a69be99d50ec7117ea67c9dca39fcf845
gobario <dlavova@mail.ru>
2023-03-09 13:19:12 +0500

    delete thrash

--- fdg/src/main/java/adad.java
+++
@@ -1,6 +0,0 @@
-public class adad {
-    public static void main(String[] args) {
-        String str = "Java";
-        System.out.println(str.charAt(2));
-    }
-}
--- homework8/src/main/java/homework8.java
+++
@@ -1,5 +0,0 @@
-public class homework8 {
-    public static void main(String[] args) {
-        new CounterApp(0);
-    }
-}
